28891bf842aaa635644ad2936151c90e325d235f
marusyaganza <marusyaganza@yandex.ru>
2018-03-14 22:32:54 +0300

    tricky

---
+++ src/tricky.js
@@ -0,0 +1,7 @@
+const str = 'kazak';
+let result = str.split('').reverse().join('') === str;
+console.log('result',result);
+const num = 5;
+console.log('is integer:', Number.isInteger(num));
+console.log('is integer:', num%1===0);
+
